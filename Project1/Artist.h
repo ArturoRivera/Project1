5e09ac46d3deec012884bdc18a563495e5245837
Unknown <arivera129@student.rcc.edu>
2017-03-01 12:57:30 -0800

    Changes to Artist Class Header

--- Project1/Artist.h
+++ Project1/Artist.h
@@ -1,8 +1,18 @@
 #pragma once
+#include<string>
+
 class Artist
 {
+
+private:
+	std::string _artistName;
+	std::string _latestSong;
+	std::string _latestAlbum;
+
 public:
 	Artist();
 	~Artist();
+
+	bool registerArtist();
 };
 
